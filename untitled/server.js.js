b4ed209021ba1839e120c296cf25ccadf9d2128e
keymoney360 <kimaniiangithua@gmail.com>
2026-09-09 11:27:00 +0300

    Add welcome message to root route
    Updated the root route to provide a welcome message.

--- untitled/server.js.js
+++ untitled/server.js.js
@@ -4,7 +4,9 @@ const cors = require('cors');
 const fs = require('fs');
 const app = express();
 const PORT = process.env.PORT || 10000;
-
+app.get("/", (req, res) => {
+  res.send("Mukatsoo iPhones API is Running ✅ - Shop at Mukatsoo");
+});
 app.use(cors());
 app.use(express.json({limit: '10mb'}));
 
