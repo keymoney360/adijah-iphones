ff623c6a8421e8603df0828c18331d51a9fcb25b
keymoney360 <kimaniiangithua@gmail.com>
2026-09-09 11:57:04 +0300

    Refactor server.js for improved structure and clarity

--- untitled/server.js.js
+++ untitled/server.js.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const path = require('path');
 const cors = require('cors');
-app.use(cors({ origin: "*" }));
 const fs = require('fs');
 const app = express();
 const PORT = process.env.PORT || 10000;
